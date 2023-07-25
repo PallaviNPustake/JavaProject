70cf7e5e29298981cea86c3fc36c806d750a00db
PallaviNPustake <pallavinpustake@gmail.com>
2023-07-25 10:05:07 +0530

    [PallaviP] This is my commit

---
+++ src/demo/Addition.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         addition(10, 30);
+		
+	}
+	public static int addition(int a,int b)
+	{
+		int sum = a+b;
+		System.out.println(sum);
+		return sum;
+	}
+
+}
